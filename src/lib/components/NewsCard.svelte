2420d0fd04f95af47a7543410ec57b4485d8cf2e
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-01-30 15:22:20 +0530

    card component

---
+++ src/lib/components/NewsCard.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	let article: {
+		source: { id: string | null; name: string };
+		author: string;
+		title: string;
+		description: string;
+		url: string;
+		urlToImage: string;
+		publishedAt: string;
+		content: string;
+	} = $props();
+</script>
+
+<article
+	class="m-4 flex flex-col overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:bg-gray-900 dark:shadow-gray-700/25 sm:flex-row"
+>
+	<img
+		alt={article.title}
+		src={article.urlToImage}
+		class="h-48 w-full object-cover sm:h-auto sm:w-1/3"
+	/>
+
+	<div class="p-4 sm:m-4 sm:w-2/3 sm:p-6">
+		<time datetime={article.publishedAt} class="block text-xs text-gray-500 dark:text-gray-400">
+			{new Date(article.publishedAt).toLocaleDateString()}
+		</time>
+
+		<a href={article.url} target="_blank" rel="noopener noreferrer">
+			<h3 class="mt-1 text-lg font-semibold text-gray-900 dark:text-white">
+				{article.title}
+			</h3>
+		</a>
+
+		<p class="mt-1 text-xs text-gray-500 dark:text-gray-400">
+			By {article.author} | Source: {article.source.name}
+		</p>
+
+		<p class="mt-2 line-clamp-3 text-sm text-gray-600 dark:text-gray-400">
+			{article.description}
+		</p>
+
+		<p class="mt-3 hidden text-sm text-gray-500 dark:text-gray-400 sm:block">
+			{article.content}
+		</p>
+	</div>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import NewsCard from '$lib/components/NewsCard.svelte';
+	let data = {
+		source: {
+			id: null,
+			name: 'Digital Journal'
+		},
+		author: 'AFP',
+		title: 'EU holds auto talks to revive embattled car sector',
+		description:
+			'EU talks to relaunch Europe’s embattled car industry are to get underway on Thursday, with automotive CEOs awaited in Brussels to discuss fines and competition from China. The European Union is under pressure to help a sector that employs 13 million people an…',
+		url: 'https://www.digitaljournal.com/world/eu-holds-auto-talks-to-revive-embattled-car-sector/article',
+		urlToImage:
+			'https://www.digitaljournal.com/wp-content/uploads/2025/01/4ac519b23d79a1d767d59398ea0dffb0cddfedc4.jpg',
+		publishedAt: '2025-01-30T08:47:35Z',
+		content:
+			"The German union IG Metall reached an agreement with Volkswagen on a cost-cutting plan to avoid forced redundancies at Europe's largest carmaker's production sites in Germany - Copyright AFP Kirill K… [+4825 chars]"
+	};
+</script>
+
+<NewsCard {...data} />
